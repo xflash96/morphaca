1eedb5c733a346dca310defc6ee5ce5cce0d77d6
b97018 <b97018@linux8.csie.ntu.edu.tw>
2012-03-24 16:47:24 +0800

    morph rewrite, polymorph isnt finished

--- polymorph/main.cpp
+++ polymorph/main.cpp
@@ -11,30 +11,44 @@
 
 using namespace cv ;
 using namespace std ;
-//using namespace Mat ;
 
 int main( int argc, char *argv[] )
 {
 	PARA para ;
-	Mat img_src, img_dst ;
-	
+	Mat *imgs ;
+	Mat warp, mix, dst_line ;
+	int dst_row, dst_col ;
+	double tmp_row, tmp_col ;
 
-	if( argc < 7 || ParseParameters( img_src, img_dst, para, argc, argv ) == 0 )
+	if( ParseParameters( imgs, para, argc, argv ) == 0 )
 	{
 		cerr << "\n" ;
-		cerr << "Usage: ./morph <src image> <dst image> <parameters>\n" ;
-		cerr << "-T	The steps during the morphing.\n" ;
-		cerr << "-SL	The lines of src image(Required)\n" ;
-		cerr << "-DL	The lines of dst image(Required)\n" ;
+		cerr << "Usage: ./polymorph <#imgs> <img1>...<imgN> <line1>...<lineN>\n" ;
 		cerr << "-a	Parameter a of warpping\n" ;
 		cerr << "-b	Parameter b of warpping\n" ;
 		cerr << "-p	Parameter p of warpping\n" ;
+		cerr << "-wi    The weight of image i, default is 1\n" ;
 		return -1 ;
 	}
 
-	time_t st = time(0);
-	fprintf(stderr, "a%lf b%lf p%lf\n", para.warp_a, para.warp_b, para.warp_p);
- 	for( int t = 0 ; t <= para.T ; t++ )
+	dst_lines = Mat::zeros( (para.lines[0]).rows, (para.lines[0]).cols, CV_32FC2 ) ;
+	tmp_row = 0 ;
+	tmp_col = 0 ;
+	for( int i = 0 ; i<para.N ; i++ )
+	{
+		dst_lines = dst_lines + para.weight[i]*para.lines[i] ;
+		tmp_row = tmp_row + para.weight[i]*imgs[i].rows ;
+		tmp_cols = tmp_cols + para.weight[i]*imgs[i].cols ;
+	}
+	dst_row = int( tmp_row+1e-7 ) ;
+	dst_col = int( tmp_col+1e-7 ) ;
+	mix = Mat::zeros( dst_rows, dst_cols, CV_32FC3 ) ;
+ 	for( int i = 0 ; i < para.N ; i++ )
+	{
+		
+	}
+
+ 	for( int i = 0 ; i < para.N ; t++ )
 	{
 		Mat src_warp = Warping( img_src, para, para.src_lines,
 		                         (1-t/para.T)*para.src_lines+t/para.T*para.dst_lines, 
@@ -46,14 +60,13 @@ int main( int argc, char *argv[] )
 					 (int)( (1-t/para.T)*(img_src.rows)+t/para.T*(img_dst.rows)+1e-7 ), 
 					 (int)( (1-t/para.T)*(img_src.cols)+t/para.T*(img_dst.cols)+1e-7 ) ) ;
 					 
-		Mat morph = CrossDissoving( src_warp, dst_warp, t/para.T ) ;
 		char s[50] ;
-		sprintf( s, "/home/student/97/b97018/htdocs/test%02d.png", t ) ;
+		sprintf( s, "test%02d.png", t ) ;
 		imwrite( s, 255*morph ) ;
 
-		time_t now = time(0);
 		fprintf(stderr, "t=%d\n", (int)(now-st));
 	}
+
 
 	return 0;
 }
